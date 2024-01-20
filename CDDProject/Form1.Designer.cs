e4bd45d313f2557333acbc2ae5bc6ad50264529c
Francisco Rochín <francisco.roching@gmail.com>
2024-01-20 16:46:15 -0700

    Add files via upload

---
+++ CDDProject/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿using System.Windows.Forms;
+
+namespace CDDProject
+{
+    partial class mainprog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainprog));
+            this.excelBtn = new System.Windows.Forms.Button();
+            this.fileBtn = new System.Windows.Forms.Button();
+            this.fileTxtb = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // excelBtn
+            // 
+            this.excelBtn.Enabled = false;
+            this.excelBtn.Location = new System.Drawing.Point(209, 101);
+            this.excelBtn.Name = "excelBtn";
+            this.excelBtn.Size = new System.Drawing.Size(170, 56);
+            this.excelBtn.TabIndex = 0;
+            this.excelBtn.Text = "Convertir a Excel";
+            this.excelBtn.UseVisualStyleBackColor = true;
+            this.excelBtn.Click += new System.EventHandler(this.excelEvent);
+            // 
+            // fileBtn
+            // 
+            this.fileBtn.Location = new System.Drawing.Point(23, 101);
+            this.fileBtn.Name = "fileBtn";
+            this.fileBtn.Size = new System.Drawing.Size(170, 56);
+            this.fileBtn.TabIndex = 1;
+            this.fileBtn.Text = "Buscar Carpeta";
+            this.fileBtn.UseVisualStyleBackColor = true;
+            this.fileBtn.Click += new System.EventHandler(this.fileEvent);
+            // 
+            // fileTxtb
+            // 
+            this.fileTxtb.Location = new System.Drawing.Point(23, 63);
+            this.fileTxtb.Name = "fileTxtb";
+            this.fileTxtb.ReadOnly = true;
+            this.fileTxtb.Size = new System.Drawing.Size(356, 20);
+            this.fileTxtb.TabIndex = 2;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(20, 30);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(359, 13);
+            this.label.TabIndex = 3;
+            this.label.Text = "Busca una carpeta con archivos de texto para convertirlos a formato Excel";
+            // 
+            // mainprog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 190);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.fileTxtb);
+            this.Controls.Add(this.fileBtn);
+            this.Controls.Add(this.excelBtn);
+            this.Name = "mainprog";
+            this.Text = "CDD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button excelBtn;
+        private System.Windows.Forms.Button fileBtn;
+        private System.Windows.Forms.TextBox fileTxtb;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ CDDProject/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace CDDProject
+{
+    public class MyCsvClassMap : ClassMap<MyDataModel>
+    {
+        public MyCsvClassMap()
+        {
+            Map(m => m.Nombre).Name("Nombre del difunto");
+            Map(m => m.Fecha).Name("Fecha").TypeConverterOption.Format("dd-MM-yyyy");
+            Map(m => m.Bloque).Name("Bloque");
+            Map(m => m.Manzana).Name("Manzana");
+            Map(m => m.Lote).Name("Lote");
+        }
+    }
+
+    public class MyDataModel
+    {
+        public string Nombre { get; set; }
+        public string Fecha { get; set; }
+        public string Bloque { get; set; }
+        public int Manzana { get; set; }
+        public string Lote { get; set; }
+    }
+
+    public partial class mainprog : Form
+    {
+        string[] files;
+        public mainprog()
+        {
+            InitializeComponent();
+        }
+
+        static void ConvertToExcel(string inputFile, string outputFolder)
+        {
+            
+            List<MyDataModel> records = new List<MyDataModel>();
+
+            using (StreamReader reader = new StreamReader(inputFile))
+            using (CsvReader csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = false }))
+            {
+                records = csv.GetRecords<MyDataModel>().ToList();
+            }
+
+            string filename = Path.GetFileNameWithoutExtension(inputFile);
+            string outputFilePath = Path.Combine(outputFolder, $"{filename}.xlsx");
+
+            using (var writer = new StreamWriter(outputFilePath))
+            using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csv.Context.RegisterClassMap<MyCsvClassMap>();
+                csv.WriteRecords(records);
+            }
+        }
+
+        private void excelEvent(object sender, EventArgs e)
+        { 
+            FolderBrowserDialog folderSaveDialog = new FolderBrowserDialog();
+            folderSaveDialog.Description = "Selecciona dónde deseas guardar los archivos.";
+
+            if (folderSaveDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in files)
+                {
+                    ConvertToExcel(file, folderSaveDialog.SelectedPath);
+                }
+                System.Windows.Forms.MessageBox.Show("¡Se han creado exitosamente los archivos!");
+            }
+
+        }
+
+        private void fileEvent(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Selecciona la carpeta que deseas convertir.";
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string dirPath = folderBrowserDialog.SelectedPath;
+                if (Directory.GetFiles(dirPath, "*.txt").Length == 0) {
+                    System.Windows.Forms.MessageBox.Show("No se encontraron archivos de texto dentro de la carpeta.");
+                } else
+                {
+                    files = Directory.GetFiles(folderBrowserDialog.SelectedPath, "*.txt", SearchOption.AllDirectories);
+                    fileTxtb.Text = dirPath;
+                    excelBtn.Enabled = true;
+                }
+            }
+        }
+
+    }
+}
